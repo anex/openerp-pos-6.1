31656cf17945c708ac5639fee210e56c849b7d01
Carlos Guerrero <guerrerocarlos@gmail.com>
2012-11-13 03:28:55 -0430

    ver ticket:71

--- addons/point_of_sale/static/src/js/pos.js
+++ addons/point_of_sale/static/src/js/pos.js
@@ -772,7 +772,7 @@ openerp.point_of_sale = function(db) {
         render_element: function() {
             this.$element.html(this.template_fct(this.model.toJSON()));
             this.select();
-            this.state.switchSign();
+            this.ShopView.NumpadView.state.switchSign();
         },
         refresh: function() {
             this.render_element();
